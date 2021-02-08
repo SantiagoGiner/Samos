8a9926664b93c6b6c060af1f87436bd72a9325c9
Santiago <santiagoginer@college.harvard.edu>
2021-02-08 16:17:17 -0400

    Fix indentation in templates and center cards

--- academy/static/academy/register.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    window.onscroll = () => {
-        if  (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
-            document.querySelector('html').style.backgroundImage = 'linear-gradient(45deg, #3c58b3 50%, #2a3c79 50%)';
-            document.querySelector('body').style.backgroundImage = 'linear-gradient(45deg, #3c58b3 50%, #2a3c79 50%)';
-        }
-    }
-})
